971fa14b79ffe3eeb515b9a7584ad6c7c8aa6314
wkang <wkang2015@163.com>
2023-08-29 21:03:42 +0800

    Remove txn package and some examples

--- examples/sqlc/mysql/schema.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists authors
-(
-    id   bigint       not null auto_increment primary key,
-    name varchar(512) not null,
-    bio  text
-);
--- examples/sqlc/pg/schema.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists authors
-(
-    id   bigserial primary key,
-    name varchar not null,
-    bio  text
-);
